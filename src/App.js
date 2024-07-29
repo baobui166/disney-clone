8ccf3ba2322f0061041a50c63c32d3fe3b16b7ee
Bui Chi Bao <buichibao1601@gmail.com>
2024-07-29 21:24:20 +0700

    viewer component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Viewer from "./components/Viewer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
-      <Viewer />
     </div>
   );
 }
--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -56,7 +56,8 @@ const Wrap = styled.div`
     width: 100%;
     height: 100%;
     border-radius: 4px;
-    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px rgb(0 0 0 / 73%) 0 16px 10px -10px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
     border: 4px solid transparent;
     transition-duration: 300ms;
 
